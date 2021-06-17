54029409cac6ffe713290faedf3a181fc8fa46be
lincj <lincj5409@gmail.com>
2021-06-17 22:35:00 +0900

    Import react-native-vector-icons lib.

--- VectorIconDemo/App.js
+++ VectorIconDemo/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text style={styles.text}>App</Text>
+      <FontAwesomeIcon name="rocket" size={30} color="#900" />
+      <FontAwesomeIcon name="home" size={30} color="#900" />
+      <MaterialIcon name="account" size={60} color="#900" />
     </SafeAreaView>
   );
 };
