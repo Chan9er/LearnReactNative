814f1b79dd050f7bb164376230d51c987cbe7eee
lincj <lincj5409@gmail.com>
2021-06-18 10:37:07 +0900

    Add drawer navigation demo src.

--- rn-navi-drawer/App.js
+++ rn-navi-drawer/App.js
@@ -1,21 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View, SafeAreaView, Alert } from 'react-native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+  useIsDrawerOpen,
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <StatusBar style="auto" />
+      <Text>HomeScreen</Text>
+      <Button
+        onPress={() =>
+          navigation.navigate('Notifications', { id: 99, name: 'taro' })
+        }
+        title="Go to notifications"
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NotificationsScreen({ navigation, route }) {
+  const isDrawerOpen = useIsDrawerOpen();
+
+  console.log('[NotificationsScreen] isDrawerOpen', isDrawerOpen);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <StatusBar style="auto" />
+      <Text>NotificationsScreen</Text>
+      <Text>Param : name={route.params.name}</Text>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
+    </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+      <DrawerItem
+        label="Toggle drawer"
+        onPress={() => props.navigation.toggleDrawer()}
+      />
+    </DrawerContentScrollView>
+    // <SafeAreaView>
+    //   <Text>CustomDrawerContent</Text>
+    // </SafeAreaView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerType="front"
+        openByDefault={false}
+        drawerStyle={{ width: '70%' }}
+        overlayColor="green"
+        drawerContentOptions={{
+          activeTintColor: 'red',
+          itemStyle: { marginTop: 8, borderWidth: 1 },
+          labelStyle: { fontSize: 16 },
+          style: { backgroundColor: 'dodgerblue' },
+        }}
+        drawerContent={props => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
