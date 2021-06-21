d73b355108bee11a6aa83c47c04c460d1189c981
lincj <lincj5409@gmail.com>
2021-06-21 12:05:38 +0900

    Add JhcNewsApp contents.

---
+++ JhcNewsApp/data/NewsItem.js
@@ -0,0 +1,12 @@
+class NewsItem {
+  constructor(id, category, title, image, pubDate, content) {
+    this.id = id;
+    this.category = category;
+    this.title = title;
+    this.image = image;
+    this.pubDate = pubDate;
+    this.content = content;
+  }
+}
+
+export default NewsItem;
